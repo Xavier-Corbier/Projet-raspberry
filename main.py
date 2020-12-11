c1b425cc979df1d69a258d32abbebd33d8d1839e
xavier <xavier.corbier@etu.umontpellier.fr>
2020-12-11 17:18:53 +0100

    Refonte Processus

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from curses.textpad import Textbox, rectangle
 
 
 
-def affichage(stdscr):
 
-        stdscr.clear()
-        stdscr.refresh()
+def main(stdscr):
 
-        while boolCondition:
-
+            stdscr.clear()
+            stdscr.refresh()
 
             # Affichage du titre du chat
             titre = 'Chat'
@@ -46,77 +44,95 @@ def affichage(stdscr):
             rectangle(stdscr, messageZone_uly, messageZone_ulx, messageZone_lry, messageZone_lrx)
             stdscr.addstr(messageZone_uly+1, 4, 'Ecrire un message :')
 
+
             #stdscr.clear()
             stdscr.refresh()
 
+            # On lance les 3 processus
+            for i in range(1):
+
+                # On fork un processus
+                pid = os.fork()
+
+                # Si je suis un fils
+                if pid == 0:
+
+                    # Si je suis le premier
+                    if i==0 :
+                        curses.wrapper(lectureMessage)
+                        sys.exit(i)
+                    if i==1 :
+                        curses.wrapper(affichageMessage)
+                        sys.exit(i)
+                # On tue tout les fils
+            for i in range(1):
+                pid,status = os.wait()
+
+
 def lectureMessage(stdscr):
+
+        messageZone_uly = (int(stdscr.getmaxyx()[0] * 0.9 + 1))
+        messageZone_ulx = (int(stdscr.getbegyx()[1])+2)
+        messageZone_lrx = (int(stdscr.getmaxyx()[1] * 0.8 - 2))
 
         while boolCondition :
 
-            messageZon_uly = (int(stdscr.getmaxyx()[0] * 0.9 + 1))
-            messageZon_ulx = (int(stdscr.getbegyx()[1])+2)
-            messageZon_lry = (int(stdscr.getmaxyx()[0] - 1))
-            messageZon_lrx = (int(stdscr.getmaxyx()[1] * 0.8 - 2))
+            messageZone = curses.newwin(1, messageZone_lrx-4, messageZone_uly+3, messageZone_ulx+2)
 
-            messageZon = curses.newwin(1, messageZon_lrx-4, messageZon_uly+3, messageZon_ulx+2)
+            box = Textbox(messageZone)
 
-            box = Textbox(messageZon)
+
             box.edit(verificationClavier)
             message = box.gather()
             fichier = open("messages.txt", "a+")
             fichier.write("user : "+message+"\n")
             fichier.close()
+            stdscr.refresh()
             message =""
 
+def affichageMessage(stdscr):
+    global boolRepeat
+    messageZone_lry = (int(stdscr.getmaxyx()[0] - 1))
+    taille = os.path.getsize("messages.txt")
+
+    while boolCondition:
+            nouvelleTaille = os.path.getsize("messages.txt")
+            if  nouvelleTaille != taille :
+                taille = nouvelleTaille
+
+                fichier = open("messages.txt", "r")
+                chat =fichier.read()
+                fichier.close()
+                listMessage = chat.split("\n")
+                i=0
+                while i<len(listMessage) :
+                    stdscr.addstr(messageZone_lry-5-i, 4, listMessage[len(listMessage)-i-1])
+                    stdscr.refresh()
+
+                    i+=1
 def verificationClavier(x) :
+    global boolRepeat
     # Si le programme n'est pas stoppé on renvoie la valeur qu'on a écrit
-    if boolCondition:
+    if boolCondition :
         return x
     # Sinon on renvoie une valeur de fin (Simulation de Entrée au clavier)
     else :
+        boolRepeat = False
         return 10
+
 
 def interruptionProgramme(signum, frame):
     global boolCondition
     boolCondition = False
 
 
-
-def main():
-
-
-    # On lance les 3 processus
-    for i in range(3):
-
-        # On fork un processus
-        pid = os.fork()
-
-        # Si je suis un fils
-        if pid == 0:
-
-            # Si je suis le premier
-            if i==0 :
-                curses.wrapper(affichage)
-                sys.exit(i)
-            elif i==1 :
-                curses.wrapper(lectureMessage)
-                sys.exit(i)
-            elif i==2 :
-                sys.exit(i)
-
-    # On tue tout les fils
-    for i in range(3):
-        pid,status = os.wait()
-
-
-
 if __name__ == "__main__":
 
     # Variable global qui gère l'activité du programme
     boolCondition = True
-
+    boolRepeat = True
     # Si un signal d'interruption est reçu , on stoppe le programme
     signal.signal(signal.SIGINT, interruptionProgramme)
 
     # Lancement de la fonction principale
-    main()
+    curses.wrapper(main)
