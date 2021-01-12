3c2e3d2395445af51b3b45a384f757b7828d23a2
xavier.corbier <xavier.corbier@etu.umontpellier.fr>
2021-01-12 13:55:34 +0100

    Correction lien

--- plugins/pluginGestionUtilisateurs.py
+++ plugins/pluginGestionUtilisateurs.py
@@ -16,8 +16,8 @@ class GestionUtilisateurs(object):
             # Récupération des utilisateurs actuels
             listeUtilisateurs = self.listeDesUtilisateurs()
             # Réécriture de tous les noms d'utilisateurs sauf celui que l'on veut supprimer
-            lienDossier = os.path.dirname(os.getcwd())
-            fichier = open(os.path.join(lienDossier, "Projet-raspberry/données/utilisateurs.txt"), "w")
+            lienDossier = os.path.dirname(os.path.realpath('__file__'))
+            fichier = open(os.path.join(lienDossier, "données/utilisateurs.txt"), "w")
             nombreSupprime = 0
             for user in listeUtilisateurs:
                 if nombreSupprime == 0:
@@ -39,8 +39,8 @@ class GestionUtilisateurs(object):
         # TO DO : Pas sur du fonctionnement à verifier !
         #with ILock('utilisateursAjout'):
             # Ecriture du nom d'utilisateur dans le fichier
-            lienDossier = os.path.dirname(os.getcwd())
-            fichier = open(os.path.join(lienDossier, "Projet-raspberry/données/utilisateurs.txt"), "a+")
+            lienDossier = os.path.dirname(os.path.realpath('__file__'))
+            fichier = open(os.path.join(lienDossier, "données/utilisateurs.txt"), "a+")
             fichier.write(nomUtilisateur+"\n")
             fichier.close()
 
@@ -51,8 +51,8 @@ class GestionUtilisateurs(object):
         # TO DO : Pas sur du fonctionnement à verifier !
         #with ILock('utilisateursLecture'):
             # Lecture du fichier
-            lienDossier = os.path.dirname(os.getcwd())
-            fichier = open(os.path.join(lienDossier, "Projet-raspberry/données/utilisateurs.txt"), "r")
+            lienDossier = os.path.dirname(os.path.realpath('__file__'))
+            fichier = open(os.path.join(lienDossier, "données/utilisateurs.txt"), "r")
             utilisateurs =fichier.read()
             fichier.close()
             # Décomposition du fichier en ligne
