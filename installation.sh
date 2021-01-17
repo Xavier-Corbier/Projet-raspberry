6c29eb7958349d09900cc843afac4b94a7069aac
xavier <xavier.corbier@etu.umontpellier.fr>
2021-01-17 14:17:41 +0100

    Test programme installation

--- installation.sh
+++ installation.sh
@@ -3,32 +3,6 @@ commande=$(sudo -nv 2>&1)
 if [ $? -eq 0 ]; then
 ## il n'est pas nécésaire de demander le mot de passe pour avoir l'accès sudoers
 echo "Installation en cours ..."
-  ## vérification si git est installé
-  if which git | grep '^/'; then
-    ## cacher le résultat du git clone
-    echo "Téléchargement du programme chat ..."
-  else
-    ## la commande git n'est pas présente sur l'os
-    echo "Installation programmes nécéssaire"
-    sudo apt install git-all
-    ## Fin installation programme nécéssaire
-    echo "Installation programme nécéssaire fini"
-    ## cacher le résultat du git clone
-    echo "Téléchargement du programme chat ..."
-  fi
-  ## vérification si python3 est installé
-  if which python3 | grep '^/'; then
-    ## cacher le résultat du git clone
-    echo "Téléchargement du programme chat ..."
-  else
-    ## la commande python3 n'est pas présente sur l'os
-    echo "Installation programmes nécéssaire"
-    sudo apt install python3
-    ## Fin installation programme nécéssaire
-    echo "Installation programme nécéssaire fini"
-    ## cacher le résultat du git clone
-    echo "Téléchargement du programme chat ..."
-  fi
   git clone https://github.com/Xavier-Corbier/Projet-raspberry.git > /dev/null 2>&1
   ## création d'un utilisateur chat sur la raspberry sans mot de passe et sans interractiveté avec le terminal
   sudo adduser --disabled-password --gecos "" chat > /dev/null 2>&1
