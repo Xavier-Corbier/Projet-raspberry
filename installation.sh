84899895e1d69dd757994a5e50f192407e05b4cc
xavier <xavier.corbier@etu.umontpellier.fr>
2021-01-17 01:48:20 +0100

    Test programme installation

---
+++ installation.sh
@@ -0,0 +1,29 @@
+telechargementEtInstallationProgramme(){
+  # clone du projet
+  git clone https://github.com/Xavier-Corbier/Projet-raspberry.git > /dev/null 2>&1
+
+}
+
+commande=$(sudo -nv 2>&1)
+## exécute la commande et vérifie qu'elle s'est éxécuté avec succés
+if [ $? -eq 0 ]; then
+## il n'est pas nécésaire de demander le mot de passe pour avoir l'accès sudoers
+echo "Installation en cours ..."
+  if which git | grep -q '^/'; then
+    ## cacher le résultat du git clone
+    echo "Téléchargement du programme chat ..."
+    telechargementEtInstallation
+  else
+    ## la commande git n'est pas présente sur l'os
+    echo "Installation programme nécéssaire"
+    sudo apt install git-all
+    telechargementEtInstallation
+  fi
+elif echo $commande | grep -q '^sudo:'; then
+## il est nécéssaire de demander le mot de passe pour avoir l'accès sudoers
+echo "Erreur : Veuillez vérifier que vous avez tapper sudo avant la commande d'installation svp"
+else
+## l'utilisateur n'a pas les droits sudoers
+echo "Erreur : Vous n'avez pas les droits administrateurs"
+fi
+
