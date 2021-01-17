2f5c5e2652de83e747b04266785992ac7dc467a8
xavier <xavier.corbier@etu.umontpellier.fr>
2021-01-17 02:04:47 +0100

    Test programme installation

--- installation.sh
+++ installation.sh
@@ -16,6 +16,10 @@ echo "Installation en cours ..."
     echo "Téléchargement du programme chat ..."
   fi
   git clone https://github.com/Xavier-Corbier/Projet-raspberry.git > /dev/null 2>&1
+  ## Création d'un utilisateur chat sur la raspberry sans mot de passe et sans interractiveté avec le terminal
+  sudo adduser --disabled-password --gecos "" chat > /dev/null 2>&1
+
+
 elif echo $commande | grep -q '^sudo:'; then
 ## il est nécéssaire de demander le mot de passe pour avoir l'accès sudoers
 echo "Erreur : Veuillez vérifier que vous avez tapper sudo avant la commande d'installation svp"
