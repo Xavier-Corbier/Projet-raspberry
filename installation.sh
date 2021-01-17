14b559d5c2109e5151c577c20d655bfa668cfd12
xavier <xavier.corbier@etu.umontpellier.fr>
2021-01-17 02:33:15 +0100

    Test programme installation

--- installation.sh
+++ installation.sh
@@ -3,6 +3,7 @@ commande=$(sudo -nv 2>&1)
 if [ $? -eq 0 ]; then
 ## il n'est pas nécésaire de demander le mot de passe pour avoir l'accès sudoers
 echo "Installation en cours ..."
+  ## vérification si git est installé
   if which git | grep -q '^/'; then
     ## cacher le résultat du git clone
     echo "Téléchargement du programme chat ..."
@@ -10,6 +11,19 @@ echo "Installation en cours ..."
     ## la commande git n'est pas présente sur l'os
     echo "Installation programmes nécéssaire"
     sudo apt install git-all
+    ## Fin installation programme nécéssaire
+    echo "Installation programme nécéssaire fini"
+    ## cacher le résultat du git clone
+    echo "Téléchargement du programme chat ..."
+  fi
+  ## vérification si python3 est installé
+  if which python3 | grep -q '^/'; then
+    ## cacher le résultat du git clone
+    echo "Téléchargement du programme chat ..."
+  else
+    ## la commande python3 n'est pas présente sur l'os
+    echo "Installation programmes nécéssaire"
+    sudo apt install python3
     ## Fin installation programme nécéssaire
     echo "Installation programme nécéssaire fini"
     ## cacher le résultat du git clone
@@ -24,6 +38,9 @@ echo "Installation en cours ..."
   sudo cp /home/chat/Projet-raspberry/chat /usr/bin/chat
   ## autorisation d'éxécuter la commande chat
   sudo chmod 777 /usr/bin/chat
+  ## autorisation d'écritures de données
+  sudo chmod 777 /home/chat/Projet-raspberry/données/messages.txt
+  sudo chmod 777 /home/chat/Projet-raspberry/données/utilisateurs.txt
   echo "Installation terminé !"
 
 elif echo $commande | grep -q '^sudo:'; then
