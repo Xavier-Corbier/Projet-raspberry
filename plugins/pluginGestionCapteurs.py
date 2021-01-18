d76c5fad04e26bd6bc2fec7512c95c3f77f2b068
xavier <xavier.corbier@etu.umontpellier.fr>
2021-01-18 22:55:47 +0100

    ajout commentaires

--- plugins/pluginGestionCapteurs.py
+++ plugins/pluginGestionCapteurs.py
@@ -2,7 +2,13 @@ from drivers.driverLED import *
 from drivers.driverLCD import *
 from drivers.driverBouton import *
 import time
+
+# Class qui permet de gérer les capteurs
+
 class GestionCapteurs(object):
+    # allume une LED Si elle est branché
+    # Résultat :
+    # - La LED est allumé 3 s si elle est branché
     def alumerLed(self):
         try :
             initLED()
@@ -12,12 +18,17 @@ class GestionCapteurs(object):
         except Exception:
             pass
 
+    # allume une LED Si elle est branché
+    # Résultat :
+    # - La LED est allumé 3 s si elle est branché
     def afficherMessage(self, message):
         try :
             setTextLigneParLigne(message)
         except Exception:
             pass
-
+    # vérifie si un bouton est appuyé
+    # Résultat :
+    # - Un booléen à True si le bouton est branché et appuyé , sinon False
     def boutonEstActif(self):
         try :
             initBouton()
